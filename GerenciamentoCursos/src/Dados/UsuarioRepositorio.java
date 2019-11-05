3cf71e55dc694b3870e1decfe9f7f9f2ed519a4a
unknown <bruno.soliveira@LAB-DEINFO-17.deinfo.lab33>
2019-11-05 18:49:14 -0300

    Atualizando Usuarios Repositorio

--- GerenciamentoCursos/src/Dados/UsuarioRepositorio.java
+++ GerenciamentoCursos/src/Dados/UsuarioRepositorio.java
@@ -1,6 +1,7 @@
 package Dados;
 
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
@@ -26,10 +27,10 @@ public class UsuarioRepositorio {
 	public UsuarioRepositorio(){
 		listaUsuario = new ArrayList <Usuario> ();
 	}
-	
+
 	public void encontrarTodosUsuarios() {
 		listaUsuario.clear();
-		String sql = "SELECT * FROM usuario";
+		String sql = "SELECT * FROM usuario WHERE ativo = 'S'";
 		Conexao.getInstance().buscarSQL(sql);
 
 		try {
@@ -41,7 +42,7 @@ public class UsuarioRepositorio {
 						Conexao.getInstance().getResultset().getString("ativo").charAt(0));
 				listaUsuario.add(usuario);
 			}
-			
+			Conexao.getInstance().setResultset(null);
 			for (Usuario user: listaUsuario) {
 				System.out.println(user.getNome());
 			}
@@ -55,20 +56,21 @@ public class UsuarioRepositorio {
 	}
 
 	public void encontrarUsuario(String cpf) {
-		listaUsuario.clear();
-		String sql = "SELECT * FROM usuario WHERE cpf = "+cpf;
-		Conexao.getInstance().buscarSQL(sql);
-
 		try {
-			while (Conexao.getInstance().getResultset().next()) {
-				usuario = new Usuario(Conexao.getInstance().getResultset().getString("cpf"), Conexao.getInstance().getResultset().getString("nome"),
-						Conexao.getInstance().getResultset().getString("email"), Conexao.getInstance().getResultset().getString("sexo").charAt(0), 
-						Conexao.getInstance().getResultset().getString("telefone"),
-						Conexao.getInstance().getResultset().getString("senha"),
-						Conexao.getInstance().getResultset().getString("ativo").charAt(0));
-				listaUsuario.add(usuario);
-				System.out.println(this.usuario.getNome());
-				Conexao.getInstance().setResultset(null);
+			if (cpf != null && !cpf.isEmpty()) {
+				listaUsuario.clear();
+				String sql = "SELECT * FROM usuario WHERE cpf = "+cpf;
+				Conexao.getInstance().buscarSQL(sql);
+				while (Conexao.getInstance().getResultset().next()) {
+					usuario = new Usuario(Conexao.getInstance().getResultset().getString("cpf"), Conexao.getInstance().getResultset().getString("nome"),
+							Conexao.getInstance().getResultset().getString("email"), Conexao.getInstance().getResultset().getString("sexo").charAt(0), 
+							Conexao.getInstance().getResultset().getString("telefone"),
+							Conexao.getInstance().getResultset().getString("senha"),
+							Conexao.getInstance().getResultset().getString("ativo").charAt(0));
+					listaUsuario.add(usuario);
+					System.out.println(this.usuario.getNome());
+					Conexao.getInstance().setResultset(null);
+				}
 			}
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
@@ -88,8 +90,8 @@ public class UsuarioRepositorio {
 			for (Object obj: data) {
 				if (obj == null) {
 					JOptionPane.showMessageDialog(null, "Parâmetros incompletos", "Erro", JOptionPane.ERROR_MESSAGE);
-				} else if(o != obj){
-					if(obj instanceof Character) {
+				} else if(o != obj) {
+					if (obj instanceof Character) {
 						//tipo char concatena com aspas simples
 						sql += "\'" + obj + "\', "; 
 					} else {
@@ -101,7 +103,7 @@ public class UsuarioRepositorio {
 				}
 			}
 			sql += ");";
-			System.out.println(sql);
+
 			int rowInsered = Conexao.getInstance().executaSQL(sql);
 			if (rowInsered == 200) {
 				JOptionPane.showMessageDialog(null, "Usuário inserido com sucesso");
@@ -109,6 +111,65 @@ public class UsuarioRepositorio {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	public void atualizarUsuario(String cpf, String nome, String telefone, String email, String senha) {
+		try {
+			if (cpf != null &&  cpf.isEmpty()) {
+				String sql = "UPDATE usuario SET";
+				int aux = 0;
+				if (nome != null && !nome.isEmpty()) {
+					sql += " nome = '" + nome + "'";
+					aux = 1;
+				}
+
+				if (telefone != null && !telefone.isEmpty()) {
+					if (aux == 1) {
+						sql += ", telefone = '" + telefone + "'";
+					} else {
+						aux = 1;
+						sql += " telefone = '" + telefone + "'";
+					}
+				}
+
+				if (email != null && !email.isEmpty()) {
+					if (aux == 1) {
+						sql += ", email = '" + email + "'";
+					} else {
+						sql += " email = '" + email + "'";
+					}
+				}
+
+				sql += " WHERE cpf = '" + cpf + "'";
+
+				int rowInsered = Conexao.getInstance().executaSQL(sql);
+				if (rowInsered == 200) {
+					JOptionPane.showMessageDialog(null, "Usuário atualizado com sucesso");
+				} else {
+					JOptionPane.showMessageDialog(null, "Parâmetros incompletos", "Erro", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void desativarUsuario(String cpf) {
+		try {
+			if (cpf != null && !cpf.isEmpty()) {
+				String sql = "UPDATE usuario SET ativo = 'N' WHERE cpf = '" + cpf + "'";
+				
+				int rowInsered = Conexao.getInstance().executaSQL(sql);
+				
+				if (rowInsered == 200) {
+					JOptionPane.showMessageDialog(null, "Usuário desativado com sucesso");
+				} else {
+					JOptionPane.showMessageDialog(null, "Parâmetros incompletos", "Erro", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		}catch (Exception e) {
+			
 		}
 	}
 }
--- GerenciamentoCursos/src/Teste/Main.java
+++ GerenciamentoCursos/src/Teste/Main.java
@@ -10,50 +10,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		/*testes curso
-		//inserir
-		ArrayList<Object> myList = new ArrayList<Object>();
-
-		String cpf = "70614897409";
-		String nome = "Matheus Leite";
-		String email = "matheusantosleite@hotmail.com";
-		Character sexo = 'M';
-		String telefone = "988540180";
-		String password = "456";
-
-
-		
-		
-
-		myList.add(cpf);
-		myList.add(nome);
-		myList.add(sexo);
-		myList.add(telefone);
-		myList.add(email);
-		myList.add(password);
-		
-		UsuarioRepositorio ur = new UsuarioRepositorio();
-		Conexao.getInstance().conectar();
-		ur.inserirUsuario(myList);
-
-		
-		//encontrar 1
-		UsuarioRepositorio usr = new UsuarioRepositorio();
-		Conexao.getInstance().conectar();
-		usr.encontrarUsuario("70614897409");
-		Conexao.getInstance().desconectar();
-		
-		//encontrar todos
-		UsuarioRepositorio user = new UsuarioRepositorio();
-		Conexao.getInstance().conectar();
-		user.encontrarTodosUsuarios();
-		Conexao.getInstance().desconectar();
-		*/
-		
-		/*testes disciplina
-		 
-		 
-		 */
 		
 	}
 }
